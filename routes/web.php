e8204c480d179bbbd37995f7a8dad2837243c763
Tomas Urieles <tomasurieles31@gmail.com>
2026-02-19 11:08:03 -0500

    Grupo de rutas

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', HomeController::class);
 
 
-Route::get('/product', [ ProductController::class, 'index']);
-Route::get('/product/create', [ ProductController::class, 'create']);    
-Route::get('/product/{producto}', [ ProductController::class, 'show']);
+Route::prefix('product')->controller(ProductController::class)->group(function () {
+    Route::get('/', ['index']);
+    Route::get('/create', ['create']);    
+    Route::get('/{producto}', ['show']);
+});
+
 
 
 
